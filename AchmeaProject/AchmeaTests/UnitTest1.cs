abb3269f1403451482f8475b360e159dac1b27f0
Max Dussen <414805@student.fontys.nl>
2020-09-28 09:59:20 +0200

    Init

---
+++ AchmeaProject/AchmeaTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AchmeaTests
+{
+    //test
+    [TestClass]
+    public class UnitTest1
+    {
+        //testtesttesttest
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int hello = 1;
+
+            Assert.AreEqual(hello, 2);
+
+        }
+    }
+}
