57401a115df53cf598e627fea0f166a1c3db0e75
YannickRosheuvel <62062906+YannickRosheuvel@users.noreply.github.com>
2020-09-28 14:33:21 +0200

    -

---
+++ AchmeaProject/AchmeaProject/Controllers/ProjectController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AchmeaProject.Controllers
+{
+    public class ProjectController : Controller
+    {
+
+        //public IActionResult CreateProject(string PlaylistName)
+        //{
+
+        //}
+
+    }
+}
