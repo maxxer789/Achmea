b6648e6770f5f591d06fb19406e1a857fcc98e18
Martijn Weber <439924@student.fontys.nl>
2020-09-28 17:00:13 +0200

    UserDal Edit

--- AchmeaProject/Achmea.Core/SQL/UserDAL.cs
+++ AchmeaProject/Achmea.Core/SQL/UserDAL.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using Achmea.Core.Interface;
+using Achmea.Core.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace Achmea.Core.SQL
 {
-    class UserDAL
+    public class UserDAL : BaseDAL, IUser
     {
+        public void DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetUserByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int InsertUser(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel Login(string Email)
+        {
+            string sql = "SELECT * FROM [User] WHERE Email = @Email";
+            List<KeyValuePair<object, object>> parameters = new List<KeyValuePair<object, object>>
+            {
+                new KeyValuePair<object, object>("Email", Email)
+            };
+            DataSet result = ExecuteSQL(sql, parameters);
+            UserModel user = null;
+            if (result != null)
+            {
+                user = DatasetParser.DatasetToAccount(result, 0);
+            }
+            return user;
+        }
+
+        public void UpdateUser(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
