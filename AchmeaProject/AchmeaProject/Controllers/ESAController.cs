02f2c9b89ff74cadde35456bf7cd65fe94c1c9a3
Nick Kruize <n.kruize@gmail.com>
2020-09-28 11:51:32 +0200

    Hardcoded checklist v1

--- AchmeaProject/AchmeaProject/Controllers/ESAController.cs
+++ AchmeaProject/AchmeaProject/Controllers/ESAController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AchmeaProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AchmeaProject.Controllers
@@ -10,7 +11,17 @@ namespace AchmeaProject.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<ESA_AspectViewModel> vms = new List<ESA_AspectViewModel>()
+            {
+                new ESA_AspectViewModel(1, "ESA1", "ESA Description 1"),
+                new ESA_AspectViewModel(1, "ESA2", "ESA Description 2"),
+                new ESA_AspectViewModel(1, "ESA3", "ESA Description 3"),
+                new ESA_AspectViewModel(1, "ESA4", "ESA Description 4"),
+                new ESA_AspectViewModel(1, "ESA5", "ESA Description 5"),
+                new ESA_AspectViewModel(1, "ESA6", "ESA Description 6")
+            };
+
+            return View(vms);
         }
     }
 }
