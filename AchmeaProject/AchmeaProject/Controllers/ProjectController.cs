2dcacca908c62b9d59d92fca762f7c6274d9cc40
YannickRosheuvel <62062906+YannickRosheuvel@users.noreply.github.com>
2020-09-28 15:02:11 +0200

    ProjectDAl

--- AchmeaProject/AchmeaProject/Controllers/ProjectController.cs
+++ AchmeaProject/AchmeaProject/Controllers/ProjectController.cs
@@ -3,16 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Achmea.Core;
+using AchmeaProject.Database;
 
 namespace AchmeaProject.Controllers
 {
     public class ProjectController : Controller
     {
 
-        //public IActionResult CreateProject(string PlaylistName)
-        //{
-
-        //}
+        public IActionResult CreateProject(string PlaylistName)
+        {
+            
+        }
 
     }
 }
