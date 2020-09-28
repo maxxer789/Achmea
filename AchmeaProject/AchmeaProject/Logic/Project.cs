9a7c379f60f2f3613439b04cb2f61b84f86f0781
YannickRosheuvel <62062906+YannickRosheuvel@users.noreply.github.com>
2020-09-28 10:33:20 +0200

    folders + project aanmaken

---
+++ AchmeaProject/AchmeaProject/Logic/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AchmeaProject.Logic
+{
+    public class Project
+    {
+        public void CreateProject()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+
+        }
+    }
+}
