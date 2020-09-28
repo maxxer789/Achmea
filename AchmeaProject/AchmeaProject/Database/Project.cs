df048a083b024226dd4b311765ecf90d53a72738
YannickRosheuvel <62062906+YannickRosheuvel@users.noreply.github.com>
2020-09-28 10:35:49 +0200

    niet werkende code, commented out

--- AchmeaProject/AchmeaProject/Database/Project.cs
+++ AchmeaProject/AchmeaProject/Database/Project.cs
@@ -7,14 +7,14 @@ namespace AchmeaProject.Database
 {
     public class Project
     {
-        public void AddNewProject(string name, int ID)
-        {
-            SqlCommand cmd = new SqlCommand(@"INSERT INTO[dbo].[Playlist] (ProjectName, userID) values (@ProjectName, @userID)", con);
-            cmd.Parameters.AddWithValue("@ProjectName", name);
-            cmd.Parameters.AddWithValue("@userID", ID);
-            con.Open();
-            cmd.ExecuteNonQuery();
-            con.Close();
-        }
+        //public void AddNewProject(string name, int ID)
+        //{
+        //    SqlCommand cmd = new SqlCommand(@"INSERT INTO[dbo].[Playlist] (ProjectName, userID) values (@ProjectName, @userID)", con);
+        //    cmd.Parameters.AddWithValue("@ProjectName", name);
+        //    cmd.Parameters.AddWithValue("@userID", ID);
+        //    con.Open();
+        //    cmd.ExecuteNonQuery();
+        //    con.Close();
+        //}
     }
 }
