93a9c3e998f3d7386b0d8704c4d5b1c1b1fa789c
Martijn Weber <439924@student.fontys.nl>
2020-09-28 10:38:49 +0200

    .Core Project Set-Up

---
+++ AchmeaProject/Achmea.Core/BaseDAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Achmea.Core
+{
+    public class BaseDAL
+    {
+        private static string source = "Server=mssql.fhict.local;Database=dbi429901_achmea;User Id=dbi429901_achmea;Password=achmeagroep";
+
+        public BaseDAL(string ConnectionString)
+        {
+            source = ConnectionString;
+        }
+        public string ConnectionString
+        {
+            get
+            {
+                return source;
+            }
+        }
+
+        public DataSet ExecuteSQL(string Query, List<KeyValuePair<object, object>> Params)
+        {
+            DataSet DS = new DataSet();
+            try
+            {
+                SqlConnection Conn = new SqlConnection(ConnectionString);
+                SqlDataAdapter DA = new SqlDataAdapter();
+                SqlCommand _cmd = Conn.CreateCommand();
+
+                foreach (KeyValuePair<object, object> KVP in Params)
+                {
+                    SqlParameter Param = new SqlParameter
+                    {
+                        ParameterName = "@" + KVP.Key,
+                        Value = KVP.Value
+                    };
+                    _cmd.Parameters.Add(Param);
+                }
+                _cmd.CommandText = Query;
+                DA.SelectCommand = _cmd;
+
+                Conn.Open();
+                DA.Fill(DS);
+                Conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            return DS;
+        }
+    }
+}
