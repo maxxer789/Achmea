b5eab968b826be77ba45c8b3f7b3ed152c2b1358
YannickRosheuvel <62062906+YannickRosheuvel@users.noreply.github.com>
2020-09-28 11:03:57 +0200

    DatabaseHandler aangemaakt

---
+++ AchmeaProject/AchmeaProject/Database/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AchmeaProject.Database
+{
+    public class Project
+    {
+        public void AddNewProject(string name, int ID)
+        {
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO[dbo].[Playlist] (ProjectName, userID) values (@ProjectName, @userID)", con);
+            cmd.Parameters.AddWithValue("@ProjectName", name);
+            cmd.Parameters.AddWithValue("@userID", ID);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
