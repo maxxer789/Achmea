52ae96aed39d478b8e931dec8fc0045505ffd1dd
Nick Kruize <nick.kruize@student.fontys.nl>
2020-09-30 10:24:29 +0200

    Added AspectArea DAL/Model/Interface

---
+++ AchmeaProject/Achmea.Core/Model/AspectAreaModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Achmea.Core.Model
+{
+    public class AspectAreaModel
+    {
+        private int AspectAreaId { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+
+        public AspectAreaModel(int aspectAreaId, string title, string description)
+        {
+            AspectAreaId = aspectAreaId;
+            Title = title;
+            Description = description;
+        }
+
+        public string GetTitle()
+        {
+            return Title;
+        }
+
+        public int GetAspectArea()
+        {
+            return AspectAreaId;
+        }
+
+        public string GetDescription()
+        {
+            return Description;
+        }
+    }
+}
---
+++ AchmeaProject/Achmea.Core/SQL/AspectAreaDAL.cs
@@ -0,0 +1,23 @@
+﻿using Achmea.Core.Interface;
+using Achmea.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Achmea.Core.SQL
+{
+    public class AspectAreaDAL : BaseDAL, IAspectArea
+    {
+        public AspectAreaDAL(string Connectionstring) : base(Connectionstring)
+        {
+
+        }
+
+        public List<AspectAreaModel> GetAspectAreas()
+        {
+            List<AspectAreaModel> aspects = new List<AspectAreaModel>();
+
+            return aspects;
+        }
+    }
+}
