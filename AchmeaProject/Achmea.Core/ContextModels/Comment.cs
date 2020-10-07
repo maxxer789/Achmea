04aa537cc57c234b79640df0e696418d1c066f05
YannickRosheuvel <62062906+YannickRosheuvel@users.noreply.github.com>
2020-10-07 14:15:49 +0200

    Entity Framework toegepast

---
+++ AchmeaProject/Achmea.Core/ContextModels/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AchmeaProject.Models
+{
+    public partial class Comment
+    {
+        public int CommentId { get; set; }
+        public int FileOfProofId { get; set; }
+        public int RequirementId { get; set; }
+        public int UserId { get; set; }
+        public int ProjectId { get; set; }
+        public string Content { get; set; }
+        public DateTime? PostDateTime { get; set; }
+
+        public virtual FileOfProof FileOfProof { get; set; }
+        public virtual Project Project { get; set; }
+        public virtual SecurityRequirement Requirement { get; set; }
+        public virtual User User { get; set; }
+    }
+}
