2100371e5d38b5859b418862e8bbc2d4106209d1
YannickRosheuvel <62062906+YannickRosheuvel@users.noreply.github.com>
2020-10-26 10:15:39 +0100

    update

--- AchmeaProject/AchmeaProject/Controllers/BivController.cs
+++ AchmeaProject/AchmeaProject/Controllers/BivController.cs
@@ -24,7 +24,7 @@ namespace AchmeaProject.Controllers
         }
         public IActionResult Index()
         {
-                        List<BivViewModel> vms = ViewModelConverter.BivModelToBivViewModel(Logic.GetBiv());
+            List<BivViewModel> vms = ViewModelConverter.BivModelToBivViewModel(Logic.GetBiv());
 
             return View(vms);
         }
