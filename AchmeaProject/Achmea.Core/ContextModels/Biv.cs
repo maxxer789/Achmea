7e1bebdedab31638b805126fa5543f428fc72865
Rondael38 <58068209+Rondael38@users.noreply.github.com>
2020-10-26 14:32:54 +0100

    added models again

---
+++ AchmeaProject/Achmea.Core/ContextModels/Biv.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AchmeaProject.Models
+{
+    public partial class Biv
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ AchmeaProject/Achmea.Core/ContextModels/SecurityRequirementProject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AchmeaProject.Models
+{
+    public partial class SecurityRequirementProject
+    {
+        public int SecurityRequirementProjectId { get; set; }
+        public int ProjectId { get; set; }
+        public int SecurityRequirementId { get; set; }
+        public bool? Excluded { get; set; }
+        public string Reason { get; set; }
+        public string Status { get; set; }
+
+        public virtual Project Project { get; set; }
+        public virtual SecurityRequirement SecurityRequirement { get; set; }
+    }
+}
