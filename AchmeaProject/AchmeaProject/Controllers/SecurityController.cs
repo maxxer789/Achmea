be0414c65daf413842af08e5259b482c42a3512e
Martijn Weber <439924@student.fontys.nl>
2020-10-27 12:01:23 +0100

    security

--- AchmeaProject/AchmeaProject/Controllers/SecurityController.cs
+++ AchmeaProject/AchmeaProject/Controllers/SecurityController.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Achmea.Core;
+using Achmea.Core.Interface;
+using AchmeaProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace AchmeaProject.Controllers
 {
     public class SecurityController : Controller
     {
+        private readonly IProject Interface;
+
+        public SecurityController(IConfiguration config)
+        {
+            Interface = new ProjectDAL(config.GetConnectionString("DefaultConnection"));
+        }
+
         public IActionResult Index()
         {
             return View("Views/Accounts/Security/Index.cshtml");
+        }
+
+        public IActionResult List()
+        {
+            List<Project> list = Interface.GetProjects().ToList();
+
+            List<ProjectViewModel> listModel = new List<ProjectViewModel>();
+
+            foreach (Project ProjectModel in list)
+            {
+                ProjectViewModel ProjectVM = new ProjectViewModel()
+                {
+                    ProjectId = ProjectModel.ProjectId,
+                    Title = ProjectModel.Title,
+                    Status = ProjectModel.Status,
+                    CreationDate = ProjectModel.CreationDate.ToString()
+                };
+                if (ProjectVM.CreationDate == "1-1-0001")
+                {
+                    ProjectVM.CreationDate = "Unset";
+                }
+                listModel.Add(ProjectVM);
+            }
+            return View(listModel);
         }
     }
 }
