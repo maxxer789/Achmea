1e9b7e1c166d31971abc81cba29da1b58252879d
Max Dussen <414805@student.fontys.nl>
2020-11-02 14:33:30 +0100

    -

--- AchmeaProject/AchmeaProject/Models/ProjectDetailViewModel.cs
+++ AchmeaProject/AchmeaProject/Models/ProjectDetailViewModel.cs
@@ -14,5 +14,6 @@ namespace AchmeaProject.Models
         public string Status { get; set; }
         public string CreationDate { get; set; }
         public List<EsaAspect> EsaAspects { get; set; }
+        //h
     }
 }
