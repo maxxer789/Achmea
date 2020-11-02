8e4b38fe290112e4749cf052e518f6706d7e6126
Max Dussen <414805@student.fontys.nl>
2020-11-02 14:27:24 +0100

    AddRequirement

---
+++ AchmeaProject/AchmeaProject/Models/RequirementCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AchmeaProject.Models
+{
+    public class RequirementCreateViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Details { get; set; }
+        public string Family { get; set; }
+        public string RequirementNumber { get; set; }
+        public string MainGroup { get; set; }
+        List<int> AreaIds { get; set; }
+        List<int> BivIds { get; set; }
+    }
+}
