67d8c38c9b0a8bd5fc4541f920e6e1a19d6f09e2
Nick Kruize <n.kruize@gmail.com>
2020-11-09 13:20:09 +0100

    Retrieve file by id added, code cleanup controller

--- AchmeaProject/AchmeaProject/Controllers/EvidenceController.cs
+++ AchmeaProject/AchmeaProject/Controllers/EvidenceController.cs
@@ -4,10 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Achmea.Core.Logic;
 using AchmeaProject.Models;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Drive.v3;
 using Google.Apis.Drive.v3.Data;
+using Google.Apis.Requests;
 using Google.Apis.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -20,191 +22,32 @@ namespace AchmeaProject.Controllers
     public class EvidenceController : Controller
     {
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly DriveService service;
 
-        public EvidenceController(IWebHostEnvironment webHostEnvironment)
+        public EvidenceController(IWebHostEnvironment webHost)
         {
-            _webHostEnvironment = webHostEnvironment;
-        }
-
-        public DriveService GetDriveService()
-        {
+            _webHostEnvironment = webHost;
             string webRootPath = _webHostEnvironment.WebRootPath;
             string contentRootPath = _webHostEnvironment.ContentRootPath;
-
-            string path = Path.Combine(webRootPath, "achmea-294609-fd9d6b48f0d3.p12");
-
-            string serviceAccountEmail = "achmea@achmea-294609.iam.gserviceaccount.com";
-            DriveService service = AuthenticateServiceAccount(serviceAccountEmail, path);
-            return service;
+            service = GoogleDriveConnection.GetDriveService(webRootPath, contentRootPath);
         }
+
         public IActionResult Index()
         {
-            DriveViewModel files = new DriveViewModel();
-            files.list = ListAll(GetDriveService());
-
-            //FileList fileList = GetFileById(GetDriveService());
+            EvidenceFileViewModel files = new EvidenceFileViewModel
+            {
+                File = GoogleDriveConnection.GetFileById(service, "1NTg-K35pRhzkI2rudMRnBAL18GgsSiVL")
+            };
 
             return View(files);
         }
 
         [HttpPost]
-        public IActionResult Index(string folderName, IFormFile file)
+        public IActionResult Index(IFormFile file)
         {
-            DriveService service = GetDriveService();
-            UploadFile(service, file);
+            GoogleDriveConnection.UploadFile(service, file);
 
             return RedirectToAction("Index");
         }
-
-        public static DriveService AuthenticateServiceAccount(string serviveAccountEmail, string keyFilePath)
-        {
-            {
-                string[] scopes = new string[] { DriveService.Scope.Drive };
-
-                var certificate = new X509Certificate2(keyFilePath, "notasecret", X509KeyStorageFlags.Exportable);
-
-                try
-                {
-                    ServiceAccountCredential credential = new ServiceAccountCredential(
-                        new ServiceAccountCredential.Initializer(serviveAccountEmail)
-                        {
-                            Scopes = scopes
-                        }.FromCertificate(certificate));
-
-                    DriveService service = new DriveService(new BaseClientService.Initializer()
-                    {
-                        HttpClientInitializer = credential,
-                        ApplicationName = "Drive Api Sample"
-                    });
-                    return service;
-                }
-                catch
-                {
-                    return null;
-                }
-            }
-        }
-
-        public static string CreateFolder(DriveService service, string folderName)
-        {
-            var newFile = new File { Name = folderName, MimeType = "application/vnd.google-apps.folder" };
-
-            FilesResource.ListRequest listRequest = service.Files.List();
-            listRequest.Q = "name = 'Achmea'";
-            string folderId = listRequest.Execute().Files[0].Id;
-
-            // List files.
-            IList<File> files = listRequest.Execute().Files;
-
-            //check if foldername already exists
-
-            if (files != null && files.Count > 0)
-            {
-                foreach (var file in files)
-                {
-                    if (file.Name == newFile.Name)
-                    {
-                        return file.Id;
-                    }
-                }
-            }
-
-            newFile.Parents = new List<string> { folderId };
-            var result = service.Files.Create(newFile).Execute();
-
-            return result.Id;
-        }
-
-        public static FileList ListAll(DriveService service)
-        {
-            try
-            {
-                // Initial validation.
-                if (service == null)
-                    throw new ArgumentNullException("service");
-
-                FilesResource.ListRequest listRequest = service.Files.List();
-                listRequest.Q = "name = 'Achmea'";
-                string folderId = listRequest.Execute().Files[0].Id;
-
-                // Building the initial request.
-                var request = service.Files.List();
-                request.Q = $"'{folderId}' in parents";
-
-                FileList allfiles = request.Execute();
-                return allfiles;
-            }
-            catch (Exception Ex)
-            {
-                throw new Exception("Request Files.List failed.", Ex);
-            }
-        }
-
-        public static FileList GetFileById(DriveService service)
-        {
-            try
-            {
-                // Initial validation.
-                if (service == null)
-                    throw new ArgumentNullException("service");
-
-                FilesResource.ListRequest listRequest = service.Files.List();
-                listRequest.Q = "name = 'Achmea'";
-                string folderId = listRequest.Execute().Files[0].Id;
-
-                // Building the initial request.
-                var request = service.Files.List();
-                request.Q = $"'{folderId}' in parents and Id = '1bR_OBxgdc7EyhE2sHx-V0n9XPiBnygFN'";
-
-                FileList allfiles = request.Execute();
-                return allfiles;
-            }
-            catch (Exception Ex)
-            {
-                throw new Exception("Request Files.List failed.", Ex);
-            }
-        }
-
-        public static string UploadFile(DriveService service, IFormFile uploadFile)
-        {
-            //Get Id of parent folder Achmea for visibility purposes
-            FilesResource.ListRequest listRequest = service.Files.List();
-            listRequest.Q = "name = 'Achmea'";
-            string folderId = listRequest.Execute().Files[0].Id;
-
-            //Set File name and filetype + define parent folder
-            File Body = new File
-            {
-                Name = Path.GetFileName(uploadFile.FileName),
-                MimeType = GetMimeType(uploadFile.FileName),
-                Parents = new List<string> { folderId }
-            };
-
-            MemoryStream stream = new MemoryStream();
-            uploadFile.CopyTo(stream);
-
-            try
-            {
-                FilesResource.CreateMediaUpload request = service.Files.Create(Body, stream, GetMimeType(uploadFile.FileName));
-                request.Upload();
-                return request.ResponseBody.Id;
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
-
-        private static string GetMimeType(string fileName)
-        {
-            string mimeType = "application/unknown";
-            string ext = System.IO.Path.GetExtension(fileName).ToLower();
-            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
-            if (regKey != null && regKey.GetValue("Content Type") != null)
-                mimeType = regKey.GetValue("Content Type").ToString();
-            return mimeType;
-        }
     }
 }
-
-//"1uegokY1MYAJhQkgkOvyfB8s8DI0c5Iis"
