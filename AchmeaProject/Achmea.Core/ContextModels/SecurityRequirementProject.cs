6af594f8c9f29f43f4c2e2ba3608e0aca1d91245
Martijn Weber <439924@student.fontys.nl>
2020-12-01 11:32:08 +0100

    status edit

--- AchmeaProject/Achmea.Core/ContextModels/SecurityRequirementProject.cs
+++ AchmeaProject/Achmea.Core/ContextModels/SecurityRequirementProject.cs
@@ -1,6 +1,7 @@
 ﻿using Achmea.Core.Logic;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AchmeaProject.Models
