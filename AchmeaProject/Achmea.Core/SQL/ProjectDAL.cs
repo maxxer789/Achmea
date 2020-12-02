ff980148889146a01c0fa1eefaaab5257f735864
maxxer789 <maxvdd76@gmail.com>
2020-12-02 15:26:08 +0100

    Fix Security overview

--- AchmeaProject/Achmea.Core/SQL/ProjectDAL.cs
+++ AchmeaProject/Achmea.Core/SQL/ProjectDAL.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using Achmea.Core.ContextModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Achmea.Core
 {
@@ -126,7 +127,7 @@ namespace Achmea.Core
         public List<SecurityRequirementProject> GetRequirementsForProject(int projectId)
         {
             List<SecurityRequirementProject> requirements = new List<SecurityRequirementProject>();
-            foreach (SecurityRequirementProject reqproject in SecurityRequirementProject)
+            foreach (SecurityRequirementProject reqproject in SecurityRequirementProject.Include(p => p.FileOfProof))
             {
                 if (reqproject.ProjectId == projectId)
                 {
